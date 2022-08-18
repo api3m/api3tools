6ffab9dca106c012babffdea695ecb827653f0f7
Ryan Boder <ryanboder@tuta.io>
2022-08-18 13:07:30 -0500

    Make rrp-events a global command

--- README.md
+++ README.md
@@ -1,15 +1,57 @@
 # Airnode Tools
-Command line utils for interacting with Airnode
 
-# Setup
+Command line utils for interacting with Airnode on chain.
+
+## Setup
+
+Install the commands and the required node modules globally.
+
+```sh
+$ npm -g install
+```
+
+## RRP Events Command
+
+The rrp-events command queries a chain for Airnode RRP events and prints them to stdout as either JSON or CSV so that you can analyze them with other tools. Redirect the output to a file as needed.
+
+```sh
+$ rrp-events --help
+```
+
+### Event Types
+
+The Airnode RRP event type to query for must be given. See the event type-specific options by putting --help after the event type.
+
+```sh
+$ rrp-events full --help      # Query MadeFullRequest events
+$ rrp-events template --help  # Query MadeTemplateRequest events
+$ rrp-events fulfilled --help # Query FulfilledRequest events
+$ rrp-events failed --help    # Query FailedRequest events
+$ rrp-events sponsor --help   # Query SetSponsorshipStatus events
 
 ```
-$ npm i
+
+### Network
+
+Specify which chain/network with -n (--network). Each supported network has a config file in the networks folder that defaults to using a free RPC node. You can add/edit config files to configure your own RPC node or to add new networks.
+
+```sh
+$ rrp-events -n polygon full
 ```
 
-# Usage
+### Block Range
 
+Limit the range of blocks searched with -f (--from-block) and -t (--to-block). By default the whole chain will be searched, from block 0 to the latest block.
+
+```sh
+$ rrp-events -f 14698560 -t 14698562 full
 ```
-$ node bin/rrp-events.js --help
-$ node bin/rrp-events.js full --help
+
+### Output Format
+
+Specify the output format with -o (--output-format). By default events will be pretty-printed for the console as JSON but you can change that to raw JSON or CSV.
+
+```sh
+$ rrp-events -o json full > airnode-full-requests.json
+$ rrp-events -o csv full > airnode-full-requests.csv
 ```
--- bin/rrp-events.js
+++ bin/rrp-events.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const fs = require('fs');
 const yargs = require("yargs");
 const ethers = require("ethers");
@@ -34,31 +36,29 @@ const sponsorRequesterOptions = {
 };
 
 const args = yargs.usage("\nUsage: rrp-events <event type: full | template | fulfilled | failed | sponsor>")
-    .option("n", {alias: "network", describe: "Network: ethereum, polygon, rsk, etc...", type: "string", default: "ethereum" })
-    .option("f", {alias: "from-block", describe: "From block number", type: "number", default: 0 })
-    .option("t", {alias: "to-block", describe: "To block number", type: "number", default: "latest" })
-    .option("o", {alias: "output", describe: "Output format", type: "string", default: "console" })
+    .option("n", { alias: "network", describe: "Network: ethereum, polygon, rsk, etc...", type: "string", default: "ethereum" })
+    .option("f", { alias: "from-block", describe: "From block number", type: "number", default: 0 })
+    .option("t", { alias: "to-block", describe: "To block number", type: "number", default: "latest" })
+    .option("o", { alias: "output", describe: "Output format", type: "string", default: "console" })
     .choices("o", ["console", "json", "csv"])
-    .command("full", "MadeFullRequest", airnodeRequestOptions)
-    .command("template", "MadeTemplateRequest", airnodeRequestOptions)
-    .command("fulfilled", "FulfilledRequest", airnodeRequestOptions)
-    .command("failed", "FailedRequest", airnodeRequestOptions)
-    .command("sponsor", "SetSponsorshipStatus", sponsorRequesterOptions)
+    .command("full", "Query MadeFullRequest events", airnodeRequestOptions)
+    .command("template", "Query MadeTemplateRequest events", airnodeRequestOptions)
+    .command("fulfilled", "Query FulfilledRequest events", airnodeRequestOptions)
+    .command("failed", "Query FailedRequest events", airnodeRequestOptions)
+    .command("sponsor", "Query SetSponsorshipStatus events", sponsorRequesterOptions)
     .demandCommand(1, "Need to specify an event type: full | template | fulfilled | failed | sponsor")
     .help(true).argv;
 
 const abi = [
     "event SetSponsorshipStatus(address indexed sponsor, address indexed requester, bool sponsorshipStatus)",
-	"event MadeTemplateRequest(address indexed airnode, bytes32 indexed requestId, uint256 requesterRequestCount, uint256 chainId, address requester, bytes32 templateId, address sponsor, address sponsorWallet, address fulfillAddress, bytes4 fulfillFunctionId, bytes parameters)",
-	"event MadeFullRequest(address indexed airnode, bytes32 indexed requestId, uint256 requesterRequestCount, uint256 chainId, address requester, bytes32 endpointId, address sponsor, address sponsorWallet, address fulfillAddress, bytes4 fulfillFunctionId, bytes parameters)",
-	"event FulfilledRequest(address indexed airnode, bytes32 indexed requestId, bytes data)",
+    "event MadeTemplateRequest(address indexed airnode, bytes32 indexed requestId, uint256 requesterRequestCount, uint256 chainId, address requester, bytes32 templateId, address sponsor, address sponsorWallet, address fulfillAddress, bytes4 fulfillFunctionId, bytes parameters)",
+    "event MadeFullRequest(address indexed airnode, bytes32 indexed requestId, uint256 requesterRequestCount, uint256 chainId, address requester, bytes32 endpointId, address sponsor, address sponsorWallet, address fulfillAddress, bytes4 fulfillFunctionId, bytes parameters)",
+    "event FulfilledRequest(address indexed airnode, bytes32 indexed requestId, bytes data)",
     "event FailedRequest(address indexed airnode, bytes32 indexed requestId, string errorMessage)"
 ];
 
 const network = JSON.parse(fs.readFileSync(`./networks/${args.network}.json`, 'utf8'));
-
 const provider = new ethers.providers.JsonRpcProvider(network.rpc);
-
 const rrp = new ethers.Contract(network.contract, abi, provider);
 
 const filters = {
@@ -69,59 +69,58 @@ const filters = {
     sponsor: rrp.filters.SetSponsorshipStatus(args.sponsor, args.requester, null)
 };
 
-const requests = rrp.queryFilter(filters[args._[0]], args.fromBlock, args.toBlock);
-
 const maps = {
     full: x => ({
         block: x.blockNumber,
-	    transaction: x.transactionHash,
-	    airnode: x.args.airnode,
-	    request: x.args.requestId,
-	    requester: x.args.requester,
-	    endpoint: x.args.endpointId,
-	    sponsor: x.args.sponsor
+        transaction: x.transactionHash,
+        airnode: x.args.airnode,
+        request: x.args.requestId,
+        requester: x.args.requester,
+        endpoint: x.args.endpointId,
+        sponsor: x.args.sponsor
     }),
     template: x => ({
         block: x.blockNumber,
-	    transaction: x.transactionHash,
-	    airnode: x.args.airnode,
-	    request: x.args.requestId,
-	    requester: x.args.requester,
-	    template: x.args.templateId,
-	    sponsor: x.args.sponsor
+        transaction: x.transactionHash,
+        airnode: x.args.airnode,
+        request: x.args.requestId,
+        requester: x.args.requester,
+        template: x.args.templateId,
+        sponsor: x.args.sponsor
     }),
     fulfilled: x => ({
         block: x.blockNumber,
-	    transaction: x.transactionHash,
-	    airnode: x.args.airnode,
-	    request: x.args.requestId,
+        transaction: x.transactionHash,
+        airnode: x.args.airnode,
+        request: x.args.requestId,
         data: x.args.data
     }),
     failed: x => ({
         block: x.blockNumber,
-	    transaction: x.transactionHash,
-	    airnode: x.args.airnode,
-	    request: x.args.requestId,
+        transaction: x.transactionHash,
+        airnode: x.args.airnode,
+        request: x.args.requestId,
         message: x.args.errorMessage
     }),
     sponsor: x => ({
         block: x.blockNumber,
-	    transaction: x.transactionHash,
-	    sponsor: x.args.sponsor,
-	    requester: x.args.requester
+        transaction: x.transactionHash,
+        sponsor: x.args.sponsor,
+        requester: x.args.requester
     })
 }
 
 formats = {
-    console: function(results) {
+    console: function (results) {
         console.log(results)
     },
-    json: function(results) {
+    json: function (results) {
         console.log(JSON.stringify(results))
     },
-    csv: async function(results) {
+    csv: async function (results) {
         console.log(await new csv(results).toString());
     }
 };
 
-requests.then(results => formats[args.output](results.map(maps[args._[0]])));
+const query = rrp.queryFilter(filters[args._[0]], args.fromBlock, args.toBlock);
+query.then(events => formats[args.output](events.map(maps[args._[0]])));
