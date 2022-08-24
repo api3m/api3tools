38a57ba4479ecadb95e66e60dd6ba03d54a68672
Ryan Boder <ryanboder@tuta.io>
2022-08-24 14:35:58 -0500

    Add remaining dAPI events

--- bin/dapilogs.js
+++ bin/dapilogs.js
@@ -22,17 +22,154 @@ logs.addEvent({
 });
 
 logs.addEvent({
+    type: "RequestedRrpBeaconUpdate",
+    alias: "rrbu",
+    options: {
+        beacon: { describe: "Filter to beacon ID" },
+        sponsor: { describe: "Filter to sponsor address" },
+        requester: { describe: "Filter to requester address" }
+    },
+    abi: "event RequestedRrpBeaconUpdate(bytes32 indexed beaconId, address indexed sponsor, address indexed requester, bytes32 requestId, address airnode, bytes32 templateId)",
+    filter: (args, f) => f.RequestedRrpBeaconUpdate(args.beacon, args.sponsor, args.requester, null, null, null),
+    map: x => ({
+        beaconId: x.beaconId,
+        sponsor: x.sponsor,
+        requester: x.requester,
+        requestId: x.requestId,
+        airnode: x.airnode,
+        templateId: x.templateId
+    })
+});
+
+logs.addEvent({
+    type: "RequestedRrpBeaconUpdateRelayed",
+    alias: "rrbur",
+    options: {
+        beacon: { describe: "Filter to beacon ID" },
+        sponsor: { describe: "Filter to sponsor address" },
+        requester: { describe: "Filter to requester address" }
+    },
+    abi: "event RequestedRrpBeaconUpdateRelayed(bytes32 indexed beaconId, address indexed sponsor, address indexed requester, bytes32 requestId, address airnode, address relayer, bytes32 templateId)",
+    filter: (args, f) => f.RequestedRrpBeaconUpdateRelayed(args.beacon, args.sponsor, args.requester, null, null, null, null),
+    map: x => ({
+        beaconId: x.beaconId,
+        sponsor: x.sponsor,
+        requester: x.requester,
+        requestId: x.requestId,
+        airnode: x.airnode,
+        relayer: x.relayer,
+        templateId: x.templateId
+    })
+});
+
+logs.addEvent({
+    type: "UpdatedBeaconWithRrp",
+    alias: "ubwr",
+    options: {
+        beacon: { describe: "Filter to beacon ID" }
+    },
+    abi: "event UpdatedBeaconWithRrp(bytes32 indexed beaconId, bytes32 requestId, int256 value, uint256 timestamp)",
+    filter: (args, f) => f.UpdatedBeaconWithRrp(args.beacon, null, null, null),
+    map: x => ({
+        beaconId: x.beaconId,
+        requestId: x.requestId,
+        value: x.value.toHexString(),
+        timestamp: x.timestamp.toNumber()
+    })
+});
+
+logs.addEvent({
+    type: "RegisteredBeaconUpdateSubscription",
+    alias: "rbus",
+    options: {
+        subscription: { describe: "Filter to subscription ID" }
+    },
+    abi: "event RegisteredBeaconUpdateSubscription(bytes32 indexed subscriptionId, address airnode, bytes32 templateId, bytes parameters, bytes conditions, address relayer, address sponsor, address requester, bytes4 fulfillFunctionId)",
+    filter: (args, f) => f.RegisteredBeaconUpdateSubscription(args.subscription, null, null, null, null, null, null, null, null),
+    map: x => ({
+        subscriptionId: x.subscriptionId,
+        airnode: x.airnode,
+        templateId: x.templateId,
+        parameters: x.parameters,
+        conditions: x.conditions,
+        relayer: x.relayer,
+        sponsor: x.sponsor,
+        requester: x.requester,
+        fulfillFunctionId: x.fulfillFunctionId
+    })
+});
+
+logs.addEvent({
+    type: "UpdatedBeaconWithPsp",
+    alias: "ubwp",
+    options: {
+        beacon: { describe: "Filter to beacon ID" }
+    },
+    abi: "event UpdatedBeaconWithPsp(bytes32 indexed beaconId, bytes32 subscriptionId, int224 value, uint32 timestamp)",
+    filter: (args, f) => f.UpdatedBeaconWithPsp(args.beacon, null, null, null),
+    map: x => ({
+        beaconId: x.beaconId,
+        subscriptionId: x.subscriptionId,
+        value: x.value.toHexString(),
+        timestamp: x.timestamp.toNumber()
+    })
+});
+
+logs.addEvent({
     type: "UpdatedBeaconWithSignedData",
     alias: "ubwsd",
     options: {
-        id: { describe: "Filter to beacon ID" }
+        beacon: { describe: "Filter to beacon ID" }
     },
     abi: "event UpdatedBeaconWithSignedData(bytes32 indexed beaconId, int256 value, uint256 timestamp)",
-    filter: (args, f) => f.UpdatedBeaconWithSignedData(args.id, null, null),
+    filter: (args, f) => f.UpdatedBeaconWithSignedData(args.beacon, null, null),
     map: x => ({
         beaconId: x.beaconId,
         value: x.value.toHexString(),
         timestamp: x.timestamp.toNumber()
+    })
+});
+
+logs.addEvent({
+    type: "UpdatedBeaconSetWithBeacons",
+    alias: "ubswb",
+    options: {
+        beaconset: { describe: "Filter to beacon set ID" }
+    },
+    abi: "event UpdatedBeaconSetWithBeacons(bytes32 indexed beaconSetId, int224 value, uint32 timestamp)",
+    filter: (args, f) => f.UpdatedBeaconSetWithBeacons(args.beaconset, null, null),
+    map: x => ({
+        beaconSetId: x.beaconSetId,
+        value: x.value.toHexString(),
+        timestamp: x.timestamp.toNumber()
+    })
+});
+
+logs.addEvent({
+    type: "UpdatedBeaconSetWithSignedData",
+    alias: "ubswsd",
+    options: {
+        dapi: { describe: "Filter to dAPI ID" }
+    },
+    abi: "event UpdatedBeaconSetWithSignedData(bytes32 indexed dapiId, int224 value, uint32 timestamp)",
+    filter: (args, f) => f.UpdatedBeaconSetWithSignedData(args.dapi, null, null),
+    map: x => ({
+        dapiId: x.dapiId,
+        value: x.value.toHexString(),
+        timestamp: x.timestamp.toNumber()
+    })
+});
+
+logs.addEvent({
+    type: "AddedUnlimitedReader",
+    alias: "aur",
+    options: {
+        reader: { describe: "Filter to reader address" }
+    },
+    abi: "event AddedUnlimitedReader(address indexed unlimitedReader)",
+    filter: (args, f) => f.AddedUnlimitedReader(args.reader),
+    map: x => ({
+        unlimitedReader: x.unlimitedReader
     })
 });
 
